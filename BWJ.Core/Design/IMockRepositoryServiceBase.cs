fc0d7b87b12b0193e079c31fcf528175695c1a9b
DESKTOP-LT9URFC\byron <byron@byronwjones.com>
2022-12-26 11:58:14 -0500

    Add and update data repository mock service

---
+++ BWJ.Core/Design/IMockRepositoryServiceBase.cs
@@ -0,0 +1,12 @@
+﻿namespace BWJ.Core.Design
+{
+    public interface IMockRepositoryServiceBase<T>
+    {
+        Task CreateRecord(T model);
+        Task DeleteRecord(Func<T, bool> predicate);
+        Task<T?> GetRecord(Func<T, bool> predicate);
+        Task<IEnumerable<T>> GetRecords();
+        Task<IEnumerable<T>> GetRecords(Func<T, bool> predicate);
+        Task UpdateRecord(Func<T, bool> predicate, T model);
+    }
+}
---
+++ BWJ.Core/Design/MockRepositoryServiceBase.cs
@@ -0,0 +1,76 @@
+﻿namespace BWJ.Core.Design
+{
+    public abstract class MockRepositoryServiceBase<T> : IMockRepositoryServiceBase<T>
+    {
+        protected readonly object @lock = new object();
+        private List<T> Data = new List<T>();
+
+        public async Task<IEnumerable<T>> GetRecords()
+        {
+            List<T> data;
+            lock (@lock)
+            {
+                data = Data;
+            }
+            return await Task.FromResult(Data);
+        }
+
+        public async Task<IEnumerable<T>> GetRecords(Func<T, bool> predicate)
+        {
+            IEnumerable<T> data;
+            lock (@lock)
+            {
+                data = Data.Where(predicate);
+            }
+
+            return await Task.FromResult(data);
+        }
+
+        public async Task<T?> GetRecord(Func<T, bool> predicate)
+        {
+            T? data;
+            lock (@lock)
+            {
+                data = Data.FirstOrDefault(predicate);
+            }
+
+            return await Task.FromResult(data);
+        }
+
+        public async Task CreateRecord(T model)
+        {
+            lock (@lock)
+            {
+                Data.Add(model);
+            }
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateRecord(Func<T, bool> predicate, T model)
+        {
+            lock (@lock)
+            {
+                var old = Data.FirstOrDefault(predicate);
+                if (old != null)
+                {
+                    Data.Remove(old);
+                    Data.Add(model);
+                }
+            }
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteRecord(Func<T, bool> predicate)
+        {
+            lock (@lock)
+            {
+                var target = Data.FirstOrDefault(predicate);
+                if (target != null)
+                {
+                    Data.Remove(target);
+                }
+            }
+            await Task.CompletedTask;
+        }
+    }
+}
