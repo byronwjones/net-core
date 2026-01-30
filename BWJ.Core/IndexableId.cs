09170fce3707c5303c39d8a9af149c4db1c5127c
DESKTOP-LT9URFC\byron <byron@byronwjones.com>
2026-01-30 11:05:31 -0500

    Index-friendly unique id generation

---
+++ BWJ.Core/IndexableId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BWJ.Core;
+public static class IndexableId
+{
+    private static DateTime EPOCH = new(2020, 1, 1);
+
+    public static string GetUniqueId()
+    {
+        var now = DateTime.UtcNow;
+        var indexPart = ((long)Math.Ceiling((now - EPOCH).TotalMilliseconds)).ToString("x10");
+        var randomPart = Guid.NewGuid().ToString("N").Substring(10);
+        var id = $"{indexPart}{randomPart}";
+        return id;
+    }
+}
